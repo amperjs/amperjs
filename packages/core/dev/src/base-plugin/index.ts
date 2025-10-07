683d1ce9594520f6588e55ebe5147126e2827096
Slee Woo <mail@sleewoo.com>
2025-10-07 20:43:59 +0300

    dev: vite plugin - createWorker

--- packages/core/dev/src/base-plugin/index.ts
+++ packages/core/dev/src/base-plugin/index.ts
@@ -23,6 +23,37 @@ import routesFactory from "./routes";
 import type { WorkerData, WorkerError, WorkerSpinner } from "./worker";
 
 export default (apiurl: string, pluginOptions?: PluginOptions): Plugin => {
+  const outDirSuffix = "client";
+
   let config: ResolvedConfig;
   let resolvedOptions: PluginOptionsResolved;
+
+  const createWorker = () => {
+    // Destructuring to separate common options from function-based ones.
+    // Functions (e.g. generators, formatters) can't be passed to worker threads.
+    const {
+      generators = [],
+      formatters = [],
+      ...restOptions
+    } = resolvedOptions;
+
+    const generatorModules: WorkerData["generatorModules"] = generators.map(
+      (e) => [e.moduleImport, e.moduleConfig],
+    );
+
+    const formatterModules: WorkerData["formatterModules"] =
+      pluginOptions?.formatters
+        ? pluginOptions.formatters.map((e) => [e.moduleImport, e.moduleConfig])
+        : [];
+
+    const workerData: WorkerData = {
+      ...restOptions,
+      generatorModules,
+      formatterModules,
+    };
+
+    return new Worker(resolve(import.meta.dirname, "base-plugin/worker.js"), {
+      workerData,
+    });
+  };
 };
