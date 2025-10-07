b7cc922f911f4a59952b51070a45e476d2ee9af0
Slee Woo <mail@sleewoo.com>
2025-10-07 06:45:11 +0300

    fetch-generator: generateLibFiles

---
+++ packages/generators/fetch-generator/src/factory.ts
@@ -0,0 +1,94 @@
+import { join } from "node:path";
+
+import {
+  defaults,
+  type GeneratorFactory,
+  pathResolver,
+  type RouteResolverEntry,
+  renderToFile,
+} from "@oreum/devlib";
+
+import fetchTpl from "./templates/fetch.hbs";
+import unwrapTpl from "./templates/unwrap.hbs";
+
+export const factory: GeneratorFactory = async ({
+  appRoot,
+  sourceFolder,
+  formatters,
+}) => {
+  const { resolve } = pathResolver({ appRoot, sourceFolder });
+
+  for (const [file, template] of [
+    // These files supposed to be replaced by specialized generators,
+    // so write them only during initialization.
+    ["unwrap.ts", unwrapTpl],
+  ]) {
+    await renderToFile(
+      resolve("fetchLibDir", file),
+      template,
+      {},
+      { formatters },
+    );
+  }
+
+  const generateLibFiles = async (entries: Array<RouteResolverEntry>) => {
+    for (const { kind, route } of entries) {
+      if (kind !== "api") {
+        continue;
+      }
+
+      await renderToFile(
+        resolve("apiLibDir", route.importPath, "fetch.ts"),
+        fetchTpl,
+        {
+          route,
+          routeMethods: route.methods.map((method) => {
+            const payloadType = route.payloadTypes.find(
+              (e) => e.method === method,
+            );
+            const responseType = route.responseTypes.find(
+              (e) => e.method === method,
+            );
+            return {
+              method,
+              payloadType,
+              responseType,
+            };
+          }),
+          paramsMapper: route.params.schema.map(({ name }, idx) => ({
+            name,
+            idx,
+          })),
+          importPathmap: {
+            core: join(defaults.appPrefix, defaults.coreDir),
+            config: join(sourceFolder, defaults.configDir),
+            fetchLib: join(sourceFolder, defaults.fetchLibDir, "lib"),
+          },
+        },
+        { formatters },
+      );
+    }
+  };
+
+  return {
+    async watchHandler(entries, event) {
+      if (event) {
+        if (event.kind === "update") {
+          await generateLibFiles(
+            entries.filter(({ kind, route }) => {
+              return kind === "api"
+                ? route.fileFullpath === event.file ||
+                    route.referencedFiles?.includes(event.file)
+                : false;
+            }),
+          );
+        }
+      } else {
+        // no event means initial call
+        await generateLibFiles(entries);
+      }
+
+      return undefined;
+    },
+  };
+};
