78a568b4dd86ea8f64dda5253ae556fa57bb664a
Slee Woo <mail@sleewoo.com>
2025-10-07 20:50:14 +0300

    dev: default exports

---
+++ packages/core/dev/src/index.ts
@@ -0,0 +1,6 @@
+export { default as apiGenerator } from "@oreum/api-generator";
+export { default as fetchGenerator } from "@oreum/fetch-generator";
+
+export { default as aliasPlugin } from "./alias-plugin";
+export { default } from "./base-plugin";
+export { default as definePlugin } from "./define-plugin";
